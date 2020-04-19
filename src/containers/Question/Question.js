27a307aae3c32131129576a26d5c8c3de331b4ab
marklawrence97 <mark_lawrence97@icloud.com>
2020-04-19 17:54:01 +0100

    add quiz functionality, 10 questions then return score

--- src/containers/Question/Question.js
+++ src/containers/Question/Question.js
@@ -7,21 +7,17 @@ export function decodeString(str) {
 
 class QuestionPage extends React.Component {
   state = {
-    question: "",
-    correct_answer: "",
-    incorrect_answers: [],
-    shuffled_answers: [],
-    selected_answer: "",
-    user_correct: false,
-    reveal_answers: false
-  };
+      question: "",
+      correct_answer: "",
+      incorrect_answers: [],
+      shuffled_answers: [],
+      selected_answer: "",
+      user_correct: false,
+      reveal_answers: false
+    };
 
-  async componentDidMount() {
-    const response = await fetch(
-      "https://opentdb.com/api.php?amount=1&category=10&difficulty=medium&type=multiple"
-    );
-    const questions = await response.json();
-    const question = questions.results[0];
+  componentDidMount() {
+    const question = this.props.question
     const decodeIncorrect = question.incorrect_answers.map((x) => {
       return decodeString(x);
     });
@@ -35,13 +31,21 @@ class QuestionPage extends React.Component {
       correct_answer: decodeString(question.correct_answer),
       incorrect_answers: decodeIncorrect,
       shuffled_answers: shuffled_answers,
+      user_correct: false,
+      reveal_answers: false
     });
+  }
+
+  componentDidUpdate() {
+    if (this.state.question != decodeString(this.props.question.question)) {
+      this.componentDidMount()
+    }
   }
 
   handleAnswerClick = (answer) => {
     if (!this.state.reveal_answers) {
         this.setState({selected_answer: answer})
-        if (answer == this.state.correct_answer) {
+        if (answer === this.state.correct_answer) {
             this.setState({user_correct: true})
             return true
         } 
@@ -59,13 +63,37 @@ class QuestionPage extends React.Component {
     }
   }
 
-  render() {
+  render() {    
     if (this.state.incorrect_answers.length === 0) {
       return <div>Loading</div>;
     }
 
     const { score } = this.props
 
+    if (this.state.reveal_answers) {
+      return (
+        <div>
+          <h1>{this.state.question}</h1>
+          {this.state.shuffled_answers.map((answer, key) => (
+            <AnswerCard 
+              answer={answer} 
+              key={key}
+              handleAnswerClick={this.handleAnswerClick}
+              selected_answer={this.state.selected_answer}
+              correct={this.state.user_correct}
+              reveal={this.state.reveal_answers}
+              correct_answer={this.state.correct_answer}/>
+          ))}
+          <button className="ui primary button" onClick={this.handleRevealAnswers}>
+              Reveal Answer!
+          </button>
+          <button className="ui button" onClick={this.props.handleNextQuestion}>
+              Next Question
+          </button>
+          <h1>Your Score: {score}</h1>
+        </div>
+      );
+    }
     return (
       <div>
         <h1>{this.state.question}</h1>
@@ -79,12 +107,12 @@ class QuestionPage extends React.Component {
             reveal={this.state.reveal_answers}
             correct_answer={this.state.correct_answer}/>
         ))}
-        <button class="ui primary button" onClick={this.handleRevealAnswers}>
+        <button className="ui primary button" onClick={this.handleRevealAnswers}>
             Reveal Answer!
         </button>
         <h1>Your Score: {score}</h1>
       </div>
-    );
+    )
   }
 }
 
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -3,7 +3,17 @@ import Question from '../Question/Question'
 
 class Quiz extends React.Component {
     state = {
-        score: 0
+        score: 0,
+        questions: [],
+        current_question: 0
+    }
+
+    async componentDidMount(){
+        const response = await fetch(
+            "https://opentdb.com/api.php?amount=10&category=10&difficulty=medium&type=multiple"
+          );
+          const questions = await response.json();
+          this.setState({questions: questions.results})
     }
 
     handleCorrectGuess = () => {
@@ -12,11 +22,38 @@ class Quiz extends React.Component {
         })
     }
 
+    handleNextQuestion = () => {
+        this.setState({
+            current_question: this.state.current_question + 1
+        })
+        console.log(this.state.current_question)
+    }
+
     render() {
+        const {questions, score, current_question} = this.state
+
+        if (questions.length === 0) {
+            return ( 
+                <div>
+                    Loading
+                </div>
+            )
+        }
+        
+        if (current_question < questions.length) {
+            return (
+                <Question 
+                    score={score}
+                    handleCorrectGuess={this.handleCorrectGuess}
+                    question={questions[current_question]}
+                    handleNextQuestion={this.handleNextQuestion}
+                    />
+            )
+        } 
         return (
-            <Question 
-                score={this.state.score}
-                handleCorrectGuess={this.handleCorrectGuess}/>
+            <div>
+                Your Score was { score }
+            </div>
         )
     }
 }
