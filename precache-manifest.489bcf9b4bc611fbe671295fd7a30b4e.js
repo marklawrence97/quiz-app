f2cae2a0dcfe8aa5cc379ad76a2227f03bba09c0
marklawrence97 <mark_lawrence97@icloud.com>
2020-04-19 18:03:07 +0100

    Updates

---
+++ precache-manifest.489bcf9b4bc611fbe671295fd7a30b4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "466f390cf101fd37c9c1376afea091fd",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "0fa9690c86a45ebc90d4",
+    "url": "/quiz-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f45e21d979bb9daf37e3",
+    "url": "/quiz-app/static/js/2.5f218468.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-app/static/js/2.5f218468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fa9690c86a45ebc90d4",
+    "url": "/quiz-app/static/js/main.4eda28a1.chunk.js"
+  },
+  {
+    "revision": "bd0b9652b41d47d00b88",
+    "url": "/quiz-app/static/js/runtime-main.54fa630c.js"
+  }
+]);
